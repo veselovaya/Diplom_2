3e5929e1c1dc9c9b8154b819a0f906a006353253
Anastasia Veselova <viviesss@yandex.ru>
2024-08-18 02:24:21 +0300

    added allure results for all tests, added requirments.txt and fullfilled README.md

--- tests/test_create_order.py
+++ tests/test_create_order.py
@@ -2,23 +2,26 @@ import pytest
 import requests
 from utils.endpoints import Urls
 from utils.data import Responses, Ingredients
+import allure
 
 class TestCreateOrder:
 
+    @allure.title('Успешное создание заказа авторизованным пользователем')
     def test_create_order_with_auth_true(self, create_and_delete_user):
         response = requests.post(f'{Urls.URL}{Urls.MAKE_ORDER}', headers={'Authorization': create_and_delete_user[2]}, data=Ingredients.valid_ingredients)
         assert response.status_code == 200 and 'true' in response.text
 
-
+    @allure.title('Успешное создание заказа неавторизованным пользователем')
     def test_create_order_no_auth_true(self):
         response = requests.post(f'{Urls.URL}{Urls.MAKE_ORDER}', data=Ingredients.valid_ingredients)
         assert response.status_code == 200 and 'true' in response.text
 
-
+    @allure.title('Неуспешное создание заказа без ингредиентов')
     def test_create_order_invalid_hash_fail(self):
         response = requests.post(f'{Urls.URL}{Urls.MAKE_ORDER}', data=Ingredients.invalid_ingredients)
         assert response.status_code == 500 and Responses.ERROR_WRONG_HASH in response.text
 
+    @allure.title('Неуспешное создание заказа с невалидным хешем')
     def test_create_order_no_ingredients_fail(self):
         response = requests.post(f'{Urls.URL}{Urls.MAKE_ORDER}')
         assert response.status_code == 400 and Responses.ERROR_EMPTY_ORDER in response.text
--- tests/test_user.py
+++ tests/test_user.py
@@ -2,20 +2,24 @@ import pytest
 import requests
 from utils.endpoints import Urls
 from utils.data import UserData, InvalidData, Responses
+import allure
 
 
 
 class TestCreateUser:
 
+    @allure.title('Успешная регистрация пользователя')
     def test_user_register_true(self):
         response = requests.post(f'{Urls.URL}{Urls.CREATE_USER}', data=UserData.create_user_data())
         assert response.status_code == 200 and 'true' in response.text
 
+    @allure.title('Создание уже существующего пользователя')
     def test_double_user_register_fail(self):
         exist_user_data = InvalidData.dublicate_data
         response = requests.post(f'{Urls.URL}{Urls.CREATE_USER}', data=exist_user_data)
         assert response.status_code == 403 and Responses.ERROR_USER_EXISTS in response.text
 
+    @allure.title('Создание пользователя с отсутствующими данными')
     @pytest.mark.parametrize(
         'invalid_data',
         [InvalidData.data_no_name, InvalidData.data_no_email, InvalidData.data_no_password]
@@ -27,10 +31,12 @@ class TestCreateUser:
 
 class TestLoginUser:
 
+    @allure.title('Успешная авторизация пользователя')
     def test_exist_user_login_true(self, create_and_delete_user):
         response = requests.post(f'{Urls.URL}{Urls.LOGIN_USER}', data=create_and_delete_user[1])
         assert response.status_code == 200 and 'true' in response.text
 
+    @allure.title('Неуспешная авторизация, если введены некорректные логин/пароль')
     @pytest.mark.parametrize(
         'mistake_data',
         [InvalidData.data_with_mistake_email, InvalidData.data_with_mistake_pass]
@@ -42,21 +48,25 @@ class TestLoginUser:
 
 class TestUpdateUser:
 
+    @allure.title('Успешное изменение имени пользователя')
     def test_update_name_with_auth_true(self,create_and_delete_user):
         new_name = {"name": UserData.generate_name()}
         response = requests.patch(f'{Urls.URL}{Urls.EDIT_USER_DATA}', headers={'Authorization':create_and_delete_user[2]}, data=new_name)
         assert response.status_code == 200 and 'true' in response.text
 
+    @allure.title('Успешное изменение емейла пользователя')
     def test_update_email_with_auth_true(self,create_and_delete_user):
         new_email = {"email": UserData.generate_email()}
         response = requests.patch(f'{Urls.URL}{Urls.EDIT_USER_DATA}', headers={'Authorization':create_and_delete_user[2]}, data=new_email)
         assert response.status_code == 200 and 'true' in response.text
 
+    @allure.title('Успешное изменение пароля пользователя')
     def test_update_pass_with_auth_true(self,create_and_delete_user):
         new_pass = {"password": UserData.generate_password()}
         response = requests.patch(f'{Urls.URL}{Urls.EDIT_USER_DATA}', headers={'Authorization':create_and_delete_user[2]}, data=new_pass)
         assert response.status_code == 200 and 'true' in response.text
 
+    @allure.title('Неуспешное изменение пользователя без авторизации')
     @pytest.mark.parametrize(
         'update_data',
         [
